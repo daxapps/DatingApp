3c13aa26e6c47046318fd5d6bdfc45cdce099b94
Jason Crawford <daxapps777@gmail.com>
2018-03-16 07:57:51 -0700

    Enable dotnet watch

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,8 +13,7 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            // return new string[] { "value1", "value2" };
-            throw new Exception("Test exception");
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
