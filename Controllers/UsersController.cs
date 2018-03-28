193ad2737d7e1b0f53988df5a3bd1ad9f3aa9dcd
Jason Crawford <daxapps777@gmail.com>
2018-03-28 10:40:55 -0700

    Define DB model with Automapper

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
@@ -12,8 +13,10 @@ namespace DatingApp.API.Controllers
     public class UsersController : Controller
     {
         private readonly IDatingRepository _repo;
-        public UsersController(IDatingRepository repo)
+        private readonly IMapper _mapper;
+        public UsersController(IDatingRepository repo, IMapper mapper)
         {
+            _mapper = mapper;
             _repo = repo;
         }
 
@@ -22,7 +25,9 @@ namespace DatingApp.API.Controllers
         {
             var users = await _repo.GetUsers();
 
-            return Ok(users);
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(usersToReturn);
         }
 
         [HttpGet("{id}")]
@@ -30,7 +35,9 @@ namespace DatingApp.API.Controllers
         {
             var user = await _repo.GetUser(id);
 
-            return Ok(user);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+
+            return Ok(userToReturn);
         }
 
     }
